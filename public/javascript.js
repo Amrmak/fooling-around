59fc96628125571594acbf764a14fbf20a66b84b
Amr Abd ElKader <amrmak@gmail.com>
2017-01-19 22:50:17 +0200

    Initial Launch

---
+++ public/javascript.js
@@ -0,0 +1,25 @@
+$('#action-button').click(function () {
+  getPostByID(2)
+});
+$('#action-button2').click(function () {
+  var result = $('input').val();
+  getPostByID(result);
+});
+function getPostByID(id) {
+  $.ajax({
+    url: 'http://localhost:3000/posts/' + id,
+    data: {
+      format: 'json'
+    },
+    error: function () {
+      $('#info').html('<p>An error has occurred</p>');
+    },
+    dataType: 'jsonp',
+    success: function (data) {
+      var result = JSON.stringify(data);
+      var $title = $('<h1>').text(result);
+      $('#info').append($title);
+    },
+    type: 'GET'
+  });
+}
